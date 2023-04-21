9b8eb310f6f8887943c1403475a874d88b7ea21a
jeballesteros09 <85072859+jeballesteros09@users.noreply.github.com>
2023-04-21 16:33:38 -0500

    Se agrega funcion para pasar token a cada endpoint

--- authentication_module_3.py
+++ authentication_module_3.py
@@ -1,9 +1,8 @@
-from flask import Flask, request, jsonify, make_response, render_template, session
+from flask import Flask, request, jsonify, make_response, render_template, session, redirect, url_for
 import jwt
 from datetime import datetime, timedelta
 from functools import wraps
 
-#  http://localhost:5000
 
 app = Flask(__name__)
 
@@ -13,14 +12,18 @@ app.config['SECRET_KEY'] = 'c09ff01bce9e4f2faafe266e5f638a39'
 def token_required(func):
     @wraps(func)
     def decorated(*args, **kwargs):
-        token = request.args.get('token')
+        token = request.cookies.get('token')
         if not token:
-            return jsonify({'Alert': 'Token is missing!'})
+            return jsonify({'Alert': 'No se encuentra el token!'})
         try:
-            payload = jwt.decode(token, app.config['SECRET_KEY'])
-        except:
-            return jsonify({'Alert': 'Invalid token'})
-        return func(*args, **kwargs)
+            payload = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
+            current_user = payload['username']
+            expiration = datetime.fromisoformat(payload['expiration'])
+            if expiration < datetime.utcnow():
+                return jsonify({'Alert': 'El token ha expirado!'})
+        except jwt.exceptions.DecodeError:
+            return jsonify({'Alert': 'Token no válido'})
+        return func(current_user, *args, **kwargs)
     return decorated
 
 
@@ -31,8 +34,8 @@ def public():
 
 @app.route('/auth')
 @token_required
-def auth():
-    return 'JWT is verified.'
+def auth(current_user):
+    return 'Listo io, tu JWT fue verificado.'
 
 
 @app.route('/')
@@ -40,7 +43,7 @@ def home():
     if not session.get('logged_in'):
         return render_template('login.html')
     else:
-        return 'Tas loggeado pa!'
+        return render_template('internal_page.html')
 
 
 @app.route('/login', methods=['POST'])
@@ -48,11 +51,13 @@ def login():
 
     if request.form['username'] == 'usuario@gmail.com' and request.form['password'] == '123456':
         session['logged_in'] = True
-        token = jwt.encode({'user': request.form['username'],
-                            'expiration': str(datetime.utcnow() + timedelta(seconds=600))}, app.config['SECRET_KEY'])
-        return jsonify({'token': token})
+        token = jwt.encode({'username': request.form['username'],
+                            'expiration': str(datetime.utcnow() + timedelta(seconds=2))}, app.config['SECRET_KEY'])
+        response = make_response(redirect(url_for('home')))
+        response.set_cookie('token', value=token, httponly=True)
+        return response
     else:
-        return make_response('Unable to verify; Wrong username or password.', 403,
+        return make_response('No se pudo verificar; Usuario o contrasena incorrectos.', 403,
                              {'WWW-Authenticate': 'Basic realm: Authentication failed!'})
 
 
